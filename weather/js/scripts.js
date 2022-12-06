297f671685742204f03271cbfc4400f5e6121707
Kylee Harward <kyleeharward21@gmail.com>
2022-12-06 13:36:39 -0700

    test

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -4,16 +4,16 @@ const todayDayNumber = d.getDay();
 
 //array for weekdays from day number
 const myweekday = new Array(7);
-myweekday[0] = "Sunday";
-myweekday[1] = "Monday";
-myweekday[2] = "Tuesday";
-myweekday[3] = "Wednesday";
-myweekday[4] = "Thursday";
-myweekday[5] = "Friday";
-myweekday[6] = "Saturday";
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
 //url for weathermap.org
-const apiURL = "//api.openweathermap.org/data/2.5/forecast?id={5781087}&appid={b05473e45b7ac25fe783c15a8fdca69a}&units=imperial";
+const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5781087&appid=b05473e45b7ac25fe783c15a8fdca69a&units=imperial";
 
 //fetch
 fetch(apiURL)
@@ -27,38 +27,36 @@ fetch(apiURL)
         //for loop and if statement
         let mylist = weatherInfo.list;
         let forecastDayNumber = todayDayNumber;
-        for (i=0; i < mylist.length; i++){
-            var time = mylist[i].dt_txt;
+        for (i = 0;i < mylist.length; i++){
+            let time = mylist[i].dt_txt;
             if (time.includes('19:00:00')){
-                
-                //increase day number each loop
-                forecastDayNumber += 1;
-                if (forecastDayNumber ===7){
-                    forecastDayNumber =0;
-                }
+
+                 //increase day number each loop
+                 forecastDayNumber += 1;
+                 if (forecastDayNumber ===7){ forecastDayNumber =0;}
 
                 //day of the week
                 let theDayName = document.createElement("span");
-                theDayName.textContent = myweekday[forecastDayNumber];
+                theDayName.textContent = weekday[forecastDayNumber];
 
                 //temp
                 let theTemp = document.createElement("p");
-                theTemp.textContent = weatherInfo[i].main.temp + "\xB0";
+                theTemp.textContent = weatherInfo.list[i].main.temp + "\xB0";
 
                  //icon
-                const iconcode = weatherInfo.list[0].weather[0].icon;
-                let iconpath = "//openweathermap.org/img/w/" + iconcode + ".png";
-                let weathericon = document.createElement('img');
-                weathericon.src = iconpath;
+                let iconcode = weatherInfo.list[i].weather[0].icon;
+                let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+                let theIcon = document.createElement('img');
+                theIcon.src = iconPath;
 
                 //append items to day div
                 let theDay = document.createElement("div");
                 theDay.appendChild(theDayName);
                 theDay.appendChild(theTemp);
-                theDay.appendChild(weathericon);
+                theDay.appendChild(theIcon);
 
                 //append day div to HTML
-                document.getElementById("weatherForecast").appendChild(theDay);
+                document.getElementById("weatherforecast").appendChild(theDay);
 
             }//end if
         }//end for
